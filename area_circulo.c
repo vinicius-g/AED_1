edfd710c6abe675e3e33544de7c963120591955f
Vinícius Gonçalves <vinicius.devlp@gmail.com>
2024-10-15 19:16:36 -0300

    segundo exercício

---
+++ area_circulo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    double raio, pi = 3.14159, area;
+    
+    scanf("%lf", &raio);
+    area = pi * (raio * raio);
+    
+    printf("A=%.4lf\n", area);
+ 
+    return 0;
+}
